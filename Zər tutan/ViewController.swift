e363b388733fa929885bc1179cf865d9692ed5e5
mirjalal <mirjalal.talishinski@gmail.com>
2019-09-19 09:10:43 +0400

    Initial Commit

---
+++ Zər tutan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zər tutan
+//
+//  Created by Mirjalal Talishinski on 9/19/19.
+//  Copyright © 2019 Mirjalal Talishinski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
