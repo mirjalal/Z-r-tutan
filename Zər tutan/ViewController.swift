177ee0c422d48f0772ce4a9bccad311795a17ab8
mirjalal <mirjalal.talishinski@gmail.com>
2019-09-19 13:25:06 +0400

    Initial commit

--- Zər tutan/ViewController.swift
+++ Zər tutan/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let dices = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6",]
+    
+    private var firstDiceIndex = 0
+    private var secondDiceIndex = 0
+    
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    
+    @IBAction func rollDices(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateDiceImages()
     }
-
-
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
+    private func updateDiceImages() {
+        firstDiceIndex = Int.random(in: 0...5)
+        firstDice.image = UIImage(named: dices[firstDiceIndex])
+        
+        secondDiceIndex = Int.random(in: 0...5)
+        secondDice.image = UIImage(named: dices[secondDiceIndex])
+    }
 }
-
